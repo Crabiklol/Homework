f39ea467e09b5eee8f3c92270ab2c358bf62dcdd
Crabiklol <kirakosyan1320@bk.ru>
2026-03-23 10:41:50 +0300

    Update chislennie.metodi.2.py

--- chislennie.metodi.2.py
+++ chislennie.metodi.2.py
@@ -79,3 +79,60 @@ plt.xlim(-2.5, 2.5)
 plt.legend(loc='upper right')
 plt.tight_layout()
 plt.show()
+
+
+
+
+
+# Домашняя работа 2. Задание 2. Вариант 8
+# Уравнение:sin x - x + 3 = 0
+# Метод: дихитомии и итерации
+
+
+import math
+
+def f(x):
+    return math.sin(x) - x + 3
+
+def g(x):
+    return math.sin(x) + 3
+
+# Метод дихотомии
+def bisection_method(a, b, tol=1e-2):
+    if f(a) * f(b) > 0:
+        print("На данном интервале нет корня")
+        return None
+    while (b - a) / 2 > tol:
+        c = (a + b) / 2
+        fc = f(c)
+        if fc == 0:
+            return c
+        elif f(a) * fc < 0:
+            b = c
+        else:
+            a = c
+    return (a + b) / 2
+
+# Метод итераций
+def iteration_method(x0, tol=1e-3, max_iter=1000):
+    x_old = x0
+    for i in range(max_iter):
+        x_new = g(x_old)
+        if abs(x_new - x_old) < tol:
+            return x_new
+        x_old = x_new
+    print("Метод итераций не сошелся")
+    return x_old
+
+
+# Локализация корня
+# У меня во время локализации корней уравнения получается 1 корень , так как на графике , на оси х идет 1 пресечение
+a, b = 0, 4  # как нашли выше
+
+root_bisect = bisection_method(a, b, tol=1e-2)
+root_iter = iteration_method(root_bisect, tol=1e-3)  # стартуем с приближенного корня
+
+
+
+print(f"Корень методом дихотомии с точностью 1e-2: {root_bisect:.4f}")
+print(f"Корень методом итераций с точностью 1e-3: {root_iter:.6f}")
